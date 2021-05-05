9df0596fe14a387d30d8ec3ad9a69f12a60b6074
Wagner Pires <wagnerpires@hotmail.com>
2021-05-05 07:35:58 -0300

    Security config

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,5 +1,6 @@
 import ImgDsDark from 'assets/img/ds_dark.svg';
 
+// function NavBar() {
 const NavBar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
