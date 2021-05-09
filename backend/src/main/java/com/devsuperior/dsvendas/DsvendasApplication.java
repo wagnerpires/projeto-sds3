856a9e461c7562aa7c1c88b8ba563f24f2c4cf91
Wagner Pires <wagnerpires@hotmail.com>
2021-05-09 12:32:25 -0300

    Correção pom.xml

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -1,12 +1,14 @@
 package com.devsuperior.dsvendas;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DsvendasApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(DsvendasApplication.class, args);
 	}
 
